185ce3b6d62973a9164858990985f3c673f51c5f
Rodrigo Gomez <Rodrigo@MacBook-Air-de-Rodrigo.local>
2020-01-08 13:03:45 -0300

    updates

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -10,7 +10,7 @@ export const Login = ({setuserName}) =>{
                             <label htmlFor="user">Nombre de Usuario</label>
                             <input type="text" className="form-control" id='user'/>
                         </div>
-                        <button type='button' className='btn btn-outline-light' onClick={e=>{
+                        <button type='submit' className='btn btn-outline-light' onClick={e=>{
                             const name= document.getElementById('user').value
                             if(name){
                                 setuserName(name)
