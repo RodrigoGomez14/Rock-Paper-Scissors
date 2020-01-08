5d6dce2745fa78671b9d2d18b66bc3acd1ceb8e5
Rodrigo Gomez <Rodrigo@MacBook-Air-de-Rodrigo.local>
2020-01-08 12:58:55 -0300

    Updates

---
+++ precache-manifest.afa26141c0a528a628db01e5c1285ca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2417b2cbbc2a6abc220dd12994407f06",
+    "url": "/Rock-Paper-Scissors/index.html"
+  },
+  {
+    "revision": "17655cf6696efd1f08c9",
+    "url": "/Rock-Paper-Scissors/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d18d701594cdcb47a88e",
+    "url": "/Rock-Paper-Scissors/static/css/main.14fe3bf0.chunk.css"
+  },
+  {
+    "revision": "17655cf6696efd1f08c9",
+    "url": "/Rock-Paper-Scissors/static/js/2.4e60291c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/Rock-Paper-Scissors/static/js/2.4e60291c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d18d701594cdcb47a88e",
+    "url": "/Rock-Paper-Scissors/static/js/main.101d05a2.chunk.js"
+  },
+  {
+    "revision": "73d72f18b5a9a769363c",
+    "url": "/Rock-Paper-Scissors/static/js/runtime-main.1bfeb084.js"
+  }
+]);
